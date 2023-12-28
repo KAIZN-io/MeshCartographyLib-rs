8968b9bad2f9766bd159d5fff726f35317952d03
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-12-28 11:37:51 +0100

    unittested

--- src/geodesic_distance/gaussian_cut_line_helper.rs
+++ src/geodesic_distance/gaussian_cut_line_helper.rs
@@ -84,7 +84,134 @@ impl MeshAnalysis {
     }
 
     fn calculate_angle(&self, a: f64, b: f64, c: f64) -> f64 {
-        let cos_angle = (b.powi(2) + c.powi(2) - a.powi(2)) / (2.0 * b * c);
+        // Check if the sides form a valid triangle
+        if a + b <= c || a + c <= b || b + c <= a {
+            return f64::NAN;
+        }
+
+        let cos_angle = ((b.powi(2) + c.powi(2) - a.powi(2)) / (2.0 * b * c)).clamp(-1.0, 1.0);
         cos_angle.acos()
+    }
+
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::io;
+    use nalgebra as na;
+    use tri_mesh::Vec3;
+
+    #[test]
+    fn test_calculate_angles_for_known_triangle() {
+        let mesh = io::load_test_mesh_closed();
+        let mesh_analysis = super::MeshAnalysis::new(mesh);
+
+        // Right-angled triangle (3-4-5 triangle)
+        let face_positions = (
+            Vec3::new(0.0, 0.0, 0.0),  // Vertex 1
+            Vec3::new(4.0, 0.0, 0.0),  // Vertex 2
+            Vec3::new(0.0, 3.0, 0.0),  // Vertex 3
+        );
+
+        let angles = mesh_analysis.calculate_angles_for_face(face_positions);
+
+        // Check that angles match (right angle and two other angles)
+        assert!((angles[0] - 0.643501).abs() < 1e-5); // Angle at vertex 1
+        assert!((angles[1] - 0.927295).abs() < 1e-5); // Angle at vertex 2 (right angle)
+        assert!((angles[2] - 1.570796).abs() < 1e-5); // Angle at vertex 3
+    }
+
+    #[test]
+    fn test_calculate_angles_for_equilateral_triangle() {
+        let mesh = io::load_test_mesh_closed();
+        let mesh_analysis = super::MeshAnalysis::new(mesh);
+
+        // Equilateral triangle (sides of length 1)
+        let face_positions = (
+            Vec3::new(0.0, 0.0, 0.0),   // Vertex 1
+            Vec3::new(1.0, 0.0, 0.0),   // Vertex 2
+            Vec3::new(0.5, 0.866, 0.0), // Vertex 3 (height of equilateral triangle)
+        );
+
+        let angles = mesh_analysis.calculate_angles_for_face(face_positions);
+
+        // All angles should be π/3 (or 60 degrees)
+        assert!((angles[0] - std::f64::consts::PI / 3.0).abs() < 1e-4);
+        assert!((angles[1] - std::f64::consts::PI / 3.0).abs() < 1e-4);
+        assert!((angles[2] - std::f64::consts::PI / 3.0).abs() < 1e-4);
+    }
+
+    #[test]
+    fn test_calculate_angles_for_isosceles_triangle() {
+        let mesh = io::load_test_mesh_closed();
+        let mesh_analysis = super::MeshAnalysis::new(mesh);
+
+        // Isosceles Triangle
+        let face_positions = (
+            Vec3::new(0.0, 0.0, 0.0), // Base Vertex 1
+            Vec3::new(3.0, 0.0, 0.0), // Base Vertex 2
+            Vec3::new(1.5, f64::sqrt(2.25), 0.0), // Apex
+        );
+
+        let angles = mesh_analysis.calculate_angles_for_face(face_positions);
+
+        assert!((angles[0] - 0.7853981633974483).abs() < 1e-6); // Base Vertex 1
+        assert!((angles[1] - 1.5707963267948968).abs() < 1e-6); // Apex
+        assert!((angles[2] - 0.7853981633974483).abs() < 1e-6); // Base Vertex 2
+    }
+
+    #[test]
+    fn test_calculate_angles_for_scalene_triangle() {
+        let mesh = io::load_test_mesh_closed();
+        let mesh_analysis = super::MeshAnalysis::new(mesh);
+
+        // Scalene Triangle
+        let face_positions = (
+            Vec3::new(0.0, 0.0, 0.0), // Vertex 1
+            Vec3::new(4.0, 0.0, 0.0), // Vertex 2
+            Vec3::new(2.0, f64::sqrt(3.0), 0.0), // Vertex 3
+        );
+
+        let angles = mesh_analysis.calculate_angles_for_face(face_positions);
+
+        assert!((angles[0] - 0.7137243789447657).abs() < 1e-6); // Vertex 1
+        assert!((angles[1] - 1.714143895700262).abs() < 1e-6); // Vertex 2
+        assert!((angles[2] - 0.7137243789447657).abs() < 1e-6); // Vertex 3
+    }
+
+    #[test]
+    fn test_calculate_angle() {
+        let mesh = io::load_test_mesh_closed();
+        let mesh_analysis = super::MeshAnalysis::new(mesh);
+
+        // Test with a known triangle (e.g., 3-4-5 triangle)
+        let angle = mesh_analysis.calculate_angle(3.0, 4.0, 5.0);
+        assert!((angle - 0.643501).abs() < 1e-6);
+
+        // Test with impossible triangle (should handle gracefully)
+        let angle = mesh_analysis.calculate_angle(10.0, 1.0, 1.0);
+        assert!(angle.is_nan());
+    }
+
+    #[test]
+    fn test_gaussian_curvature() {
+        let mesh = io::load_test_mesh_closed();
+        let mesh_analysis = super::MeshAnalysis::new(mesh);
+        let curvatures = mesh_analysis.calculate_gaussian_curvature();
+
+        // for (i, &curvature) in curvatures.iter().enumerate() {
+        //     println!("Vertex {:?} has curvature {}", mesh_analysis.mesh.position(mesh_analysis.mesh.vertex_iter().nth(i).unwrap()), curvature);
+        // }
+    }
+
+    #[test]
+    fn test_max_curvature() {
+        let mesh = io::load_test_mesh_closed();
+        let mesh_analysis = super::MeshAnalysis::new(mesh);
+        let vertex_id = mesh_analysis.vertex_with_highest_curvature();
+
+        println!("Vertex {:?} has highest curvature", mesh_analysis.mesh.position(vertex_id));
     }
 }
