cafe8ca04b13b6e76198d454a98a1502eb2eddaa
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2024-02-06 12:03:10 +0100

    building the c++ project within rust

--- build.rs
+++ build.rs
@@ -9,18 +9,25 @@
 // //! - It sets compiler flags and compiles the generated bindings.
 // //! - It includes instructions for Cargo to re-run the build script upon changes.
 
-fn main() -> miette::Result<()>  {
-    // Sets the include path for C++ headers
+fn main() -> miette::Result<()> {
+    // Existing include path for C++ headers in your Rust project
     let include_path = std::path::PathBuf::from("src/cpp");
 
-    // Initializes the `autocxx` build process
-    let mut builder = autocxx_build::Builder::new("src/lib.rs", &[&include_path])
+    // Relative path to the Eigen headers
+    let eigen_include_path = std::path::PathBuf::from("./pmp-library/external/eigen-3.4.0");
+
+    // Initializes the `autocxx` build process with both include paths
+    let mut builder = autocxx_build::Builder::new("src/lib.rs", &[&include_path, &eigen_include_path])
         .build()?;
 
     // Compiles the generated bindings with C++17 standards
-    builder.flag("-std=c++17").compile("mesh_cartography");
+    // and suppresses specific warnings
+    builder
+        .flag("-std=c++17")
+        .flag_if_supported("-Wno-unused-but-set-variable")
+        .compile("mesh_cartography");
 
-    // Instructs Cargo to re-run this script if `main.rs` changes
+    // Instructs Cargo to re-run this script if `lib.rs` changes
     println!("cargo:rerun-if-changed=src/lib.rs");
 
     Ok(())
